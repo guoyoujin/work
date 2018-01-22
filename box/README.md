8128cf0bdc31b85a7b7360e956422ad73d77841e
周维 <zhouw@tongxinyiliao.com>
2018-01-22 15:43:37 +0800

    add box conf

---
+++ box/README.md
@@ -0,0 +1,6 @@
+box
+===
+
+部署txCloudBox项目使用的docker-compose配置文件，以及初始化脚本
+
+依赖docker、docker-compose，git
---
+++ box/init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "need docker docker-compose git and code"
+
+sudo docker network create backend
+mkdir dicom
+mkdir data
