b976f5cf5376b327062bebae5154b2b5a7bf9ac7
周维 <zhouw@tongxinyiliao.com>
2018-02-06 13:07:58 +0800

    m: 重写server端和client端

--- geth/client/entrypoint.sh
+++ geth/client/entrypoint.sh
@@ -8,15 +8,16 @@ home=/home/geth
 data_path=$home/data0
 networkid=2623
 identity="zhouwei"
+bootnodes="enode://7bebecbb0a0240aae6ba213050e9bd8f07fcbf72047b9fcd32f849c23acd39d49fb8599e7ae774dca7f6ad2a8ed60ff2c1d39040302feda92cbd84ba32eb08c1@47.93.160.139:30303"
+
+geth --datadir $home/data0 init $home/genesis.json
 
 geth --identity $identity \
      --datadir $data_path \
      --networkid $networkid  \
-     --nodiscover \
      --rpc \
      --rpcport "8545" \
      --rpcapi "personal, db, eth, net, web3, miner" \
      --rpcaddr  "0.0.0.0" \
      --port "30303" \
-     --bootnodes "${BOOTNODES}"
-     console
+     --bootnodes $bootnodes 
--- geth/server/entrypoint.sh
+++ geth/server/entrypoint.sh
@@ -2,12 +2,15 @@
 # date: 2018-02-05
 # author: zhouwei
 # email: xiaomao361@163.com
-# func: start the geth server
+# func: init and start the geth server
  
 home=/home/geth
 data_path=$home/data0
 networkid=2623
 identity="zhouwei"
+log=$home/geth.log
+
+geth --datadir $home/data0 init $home/genesis.json
 
 geth --identity $identity \
      --datadir $data_path \
@@ -17,5 +20,4 @@ geth --identity $identity \
      --rpcport "8545" \
      --rpcapi "personal, db, eth, net, web3, miner" \
      --rpcaddr  "0.0.0.0" \
-     --port "30303" \
-     console
+     --port "30303" 
